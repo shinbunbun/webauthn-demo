bca0aa0736ea41ee6512fe9b30a22f3815fa32ae
shinbunbun <takamuneyuto@gmail.com>
2021-08-08 23:01:30 +0900

    add /register

--- script/index.js
+++ script/index.js
@@ -36,24 +36,18 @@ const createCredentialDefaultArgs = {
 const getCredentialDefaultArgs = {
   publicKey: {
     timeout: 60000,
-    // allowCredentials: [newCredential] // 下記参照
-    challenge: new Uint8Array([ // サーバーから暗号学的にランダムな値が送られていなければならない
-      0x79, 0x50, 0x68, 0x71, 0xDA, 0xEE, 0xEE, 0xB9, 0x94, 0xC3, 0xC2, 0x15, 0x67, 0x65, 0x26, 0x22,
-      0xE3, 0xF3, 0xAB, 0x3B, 0x78, 0x2E, 0xD5, 0x6F, 0x81, 0x26, 0xE2, 0xA6, 0x01, 0x7D, 0x74, 0x50
-    ]).buffer
+    // サーバーから暗号学的にランダムな値が送られていなければならない
+    challenge: ''
   },
 };
 
 const register = () => {
 
   // メアドと名前を取得
-  const eMail = document.querySelector('#inputEmail').value;
+  const email = document.querySelector('#inputEmail').value;
   const displayName = document.querySelector('#inputDisplayName').value;
 
-  // idを生成
-  const array = new Uint32Array(1);
-  window.crypto.getRandomValues(array);
-  const id = array[0];
+  
 
 
 
