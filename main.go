dc1c88073d738bf66e27568fc19e29d2642a6b3b
shinbunbun <takamuneyuto@gmail.com>
2021-08-08 23:01:35 +0900

    /register

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type JsonRequest struct {
+	Email       string `json:"email"`
+	DisplayName string `json:"display_name"`
+}
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*.html")
@@ -14,6 +19,15 @@ func main() {
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.POST("/register", func(ctx *gin.Context) {
+		var json JsonRequest
+		if err := ctx.ShouldBindJSON(&json); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"email": json.Email, "display_name": json.DisplayName})
 	})
 
 	router.Run(":8080")
